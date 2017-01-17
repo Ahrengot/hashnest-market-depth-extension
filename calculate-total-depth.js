f8bcaad4e057034f680e4ad02e319c7c3ec7def6
Jens Ahrengot Boddum <ahrengot@gmail.com>
2017-01-17 14:37:11 +0100

    First commit :zap:

---
+++ calculate-total-depth.js
@@ -0,0 +1,26 @@
+function ahrCalculateMarketDepth() {
+  if ( location.host.indexOf('hashnest.com') === -1 ) {
+    return console.warn("The Hashnest Market Depth extension only works on ... well, hashnest.com. You are here: ", location);
+  }
+
+  const tables = document.querySelectorAll('.market_order_table');
+  if ( !tables ) {
+    return console.warn("No market order tables found on this page.");
+  }
+
+  tables.forEach(table => {
+    // StaticNodeList to array hack: http://stackoverflow.com/a/38171853/641755
+    const btcValues = [...table.querySelectorAll('tbody td:last-child div')].map(div => {
+      return parseFloat(div.innerHTML);
+    })
+
+    const btcSum = btcValues.reduce((a,b) => {
+      return a + b;
+    }, 0);
+
+    // Update DOM
+    table.querySelector('thead th:last-child').innerHTML = `${btcSum.toFixed(2)} BTC`;
+  })
+}
+
+ahrCalculateMarketDepth();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Chrome Extension: Hashnest market depth calculator
+Shows the total market depth (in BTC) for the buy and sell orders on the hasnest.com market page.
+
+## Activating the extension
+1. Go to chrome://extensions/
+2. Load unpacked extension and select the folder
+
